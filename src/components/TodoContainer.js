27eb81f3f293f20c1f8a8f8dfc5615d6508c7f35
Sajad Rajaee <sajjadrajaee@gmail.com>
2022-08-10 23:26:22 +0430

    test react component

---
+++ src/components/TodoContainer.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+class TodoContainer extends React.Component {
+  render() {
+    return (
+      <>
+        <h1>Hello from Create React App</h1>
+        <p>I'm in React component</p>
+      </>
+    )
+  }
+}
+
+export default TodoContainer;
---
+++ src/index.js
@@ -0,0 +1,7 @@
+import React, { StrictMode } from 'react';
+import ReactDOM from 'react-dom';
+import TodoContainer from './components/TodoContainer'
+
+ReactDOM.render(<StrictMode>
+  <TodoContainer />
+  </StrictMode>,document.getElementById('root'));
